9f007e3eb9680a1ae74ce39ee7244a42af7b5b1f
nayeong kim <psi505968@gmail.com>
2024-06-04 16:56:14 +0900

    first commit

---
+++ src/main/java/com/server/esgcafe/EsgCafeApplication.java
@@ -0,0 +1,13 @@
+package com.server.esgcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsgCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/esgcafe/EsgCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.esgcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
