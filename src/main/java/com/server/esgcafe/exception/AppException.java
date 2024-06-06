090f78fee569d06a92a0cb5222e1732fd2a31cb4
nayeong kim <psi505968@gmail.com>
2024-06-06 20:03:38 +0900

    [Feat] : Exception 처리

---
+++ src/main/java/com/server/esgcafe/exception/AppException.java
@@ -0,0 +1,25 @@
+package com.server.esgcafe.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class AppException extends RuntimeException{
+
+    private ErrorCode errorCode;
+    private String message;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.message = super.getMessage();
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
---
+++ src/main/java/com/server/esgcafe/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.server.esgcafe.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 에러"),
+
+
+    ;
+
+    private HttpStatus status;
+    private String message;
+}
+
+
