5cf84e69a427f3645ac04571925a0fc7e0c5b941
kny3037 <psi505968@gmail.com>
2024-08-23 00:39:17 +0900

    [BugFix] : Add log

--- src/main/java/com/server/esgcafe/service/UserService.java
+++ src/main/java/com/server/esgcafe/service/UserService.java
@@ -22,15 +22,15 @@ public class UserService {
 
         log.info("🍞nickname : {}", request.getNickname());
 
-        Optional<User> existingUser = userRepository.findByNickName(request.getNickname());
-
-        log.info("🍞nickname length : {}", request.getNickname().length());
-
         // 닉네임의 각 문자와 유니코드 값을 출력
         for (int i = 0; i < request.getNickname().length(); i++) {
             char c = request.getNickname().charAt(i);
             log.info("🍞char at {}: '{}', Unicode: {}", i, c, (int) c);
         }
+
+        Optional<User> existingUser = userRepository.findByNickName(request.getNickname());
+
+        log.info("🍞nickname length : {}", request.getNickname().length());
 
         if (existingUser.isPresent()) {
             // 사용자가 이미 존재하는 경우 -> 로그인 처리
