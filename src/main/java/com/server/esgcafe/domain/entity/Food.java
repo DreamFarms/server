a0ab54daabe0260a79b301fca8f06d3db7c64dcb
kny3037 <psi505968@gmail.com>
2024-10-26 15:22:44 +0900

    [Rename] : file Rename and Remove whitespace

--- src/main/java/com/server/esgcafe/domain/entity/Food.java
+++ src/main/java/com/server/esgcafe/domain/entity/Food.java
@@ -19,6 +19,6 @@ public class Food {
     private String name;
 
     @OneToMany(mappedBy = "food", cascade = CascadeType.ALL)
-    private List<FoodIngredient> foodIngredients; // 음식을 만들기 위해 필요한 재료 목록
+    private List<Recipe> recipes; // 음식을 만들기 위해 필요한 재료 목록
 
 }
