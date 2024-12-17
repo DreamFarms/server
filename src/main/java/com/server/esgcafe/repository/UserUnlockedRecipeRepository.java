36120484d0c142815cb35419ecc6884b0efdae34
kny3037 <psi505968@gmail.com>
2024-12-18 00:42:15 +0900

    [Feat] : Update Ingredient Information Logic

--- src/main/java/com/server/esgcafe/repository/UserUnlockedRecipeRepository.java
+++ src/main/java/com/server/esgcafe/repository/UserUnlockedRecipeRepository.java
@@ -4,6 +4,8 @@ import com.server.esgcafe.domain.entity.Food;
 import com.server.esgcafe.domain.entity.User;
 import com.server.esgcafe.domain.entity.UserUnlockedRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -11,7 +13,8 @@ import java.util.List;
 public interface UserUnlockedRecipeRepository extends JpaRepository<UserUnlockedRecipe, Long> {
 
     // 특정 유저가 해금한 레시피 리스트를 가져오는 메서드
-    List<UserUnlockedRecipe> findByUser(User user);
+    @Query("SELECT ur FROM UserUnlockedRecipe ur JOIN FETCH ur.food WHERE ur.user = :user")
+    List<UserUnlockedRecipe> findByUserWithFood(@Param("user") User user);
 
     boolean existsByUserAndFood(User user, Food food);
 }
