6066fc25a3cb693c287c39199cc9746d15205dc1
kny3037 <psi505968@gmail.com>
2026-07-25 18:43:16 +0900

    [Bugfix] : Add annotation for logging

--- src/main/java/com/server/esgcafe/domain/dto/userInventory/UserInventorySaveRequest.java
+++ src/main/java/com/server/esgcafe/domain/dto/userInventory/UserInventorySaveRequest.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInventorySaveRequest {
